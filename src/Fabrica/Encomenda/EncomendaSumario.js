f93e5857e1b633945b607efe073983e01e0eefb4
Élio <1151557@isep.ipp.pt>
2020-01-07 00:09:21 +0000

    fix

--- src/Fabrica/Encomenda/EncomendaSumario.js
+++ src/Fabrica/Encomenda/EncomendaSumario.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const EncomendaSumario = ({ encomenda }) => {
     var cli_id = localStorage.getItem('cli_id')
-    if (localStorage.getItem('isAdmin') === "true" || encomenda.cliente[0]._id == cli_id) {
-        if (encomenda.cliente[0] != null) {
+    if (encomenda.cliente[0] != null) {
+        if (localStorage.getItem('isAdmin') === "true" || encomenda.cliente[0]._id == cli_id) {
             return (
                 <div className="card z-depth-0 project-summary">
                     <div className="card-content grey-text text-darken-3">
@@ -16,20 +16,23 @@ const EncomendaSumario = ({ encomenda }) => {
                 </div>
             )
         }
-        else {
-            return (
-                <div className="card z-depth-0 project-summary">
-                    <div className="card-content grey-text text-darken-3">
-                        <h6>Nome do cliente: UTILIZADOR APAGADO</h6>
-                        <span>Produto : {encomenda.produto[0].nome}<br /></span>
-                        <span>Quantidade : {encomenda.quantidade}<br /></span>
-                        <span>Estado : {encomenda.estado}<br /></span>
-                        <span>Data Limite : {encomenda.data_conclusao}<br /></span>
-                    </div>
-                </div>
-            )
-        }
     }
-    return null
+    else {
+        return (
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <h6>Nome do cliente: UTILIZADOR APAGADO</h6>
+                    <span>Produto : {encomenda.produto[0].nome}<br /></span>
+                    <span>Quantidade : {encomenda.quantidade}<br /></span>
+                    <span>Estado : {encomenda.estado}<br /></span>
+                    <span>Data Limite : {encomenda.data_conclusao}<br /></span>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div></div>
+    )
 }
 export default EncomendaSumario
